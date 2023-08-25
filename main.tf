6f92f7d67821f5e2fa433f46b29d96e1685fd3a9
lokesh N <104839986+bnloky@users.noreply.github.com>
2023-08-26 02:11:52 +0530

    updated with data archive

--- main.tf
+++ main.tf
@@ -4,14 +4,6 @@ provider  "aws" {
   secret_key= "SECRET_AECCESS_KEY"
 }
 
-data "archive_file" "lambda_function_archive" {
-  type        = "zip"
-  source_dir  =  "./../automation.py"  # Path to the directory containing your Lambda function code
-  output_path = "./../automation.py.zip"   # Path where you want to save the generated zip archive
-}
-
-
-
 resource "aws_lambda_function" "my_function" {
     role = "arn:aws:iam::681217613251:role/my-lambda-role"
     function_name = "my_lambda_function"
@@ -19,4 +11,14 @@ resource "aws_lambda_function" "my_function" {
     handler = "index.handler"
     timeout = 300
     filename     = "lambda_function.zip"
+
+filename = data.archive_file.lambda_function_archive.output_path
+  source_code_hash = filebase64sha256(data.archive_file.lambda_function_archive.output_path)
+
+}
+
+data "archive_file" "lambda_function_archive" {
+  type        = "zip"
+  source_dir  =  "./../automation.py"  # Path to the directory containing your Lambda function code
+  output_path = "./../automation.py.zip"   # Path where you want to save the generated zip archive
 }
