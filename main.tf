39932b889b42dfa0d6ef34d63894fc6a67d951c7
lokesh N <104839986+bnloky@users.noreply.github.com>
2023-08-26 10:53:39 +0530

    removed provideer

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider  "aws" {
-  region = "ap-south-1"
-  access_key= "ACCESS_KEY_ID"
-  secret_key= "SECRET_AECCESS_KEY"
-}
-
 resource "aws_lambda_function" "my_function" {
     role = "arn:aws:iam::681217613251:role/my-lambda-role"
     function_name = "my_lambda_function"
